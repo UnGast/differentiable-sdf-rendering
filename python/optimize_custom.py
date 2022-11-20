36a8fe9474a5170e4c9aa9deb6fcbab4a798cca2
Adrian Zimmermann <zadrian@web.de>
2022-11-20 15:07:46 +0100

    fix sensor count when existing scene is used with fewer reference images

--- python/optimize_custom.py
+++ python/optimize_custom.py
@@ -19,6 +19,8 @@ def optimize(config, opt_name, output_dir, ref_image_paths, ref_spp=1024,
     #render_reference_images(opt_config, config, ref_spp=ref_spp, force=force, verbose=verbose, mts_args=mts_args)
     #ref_image_paths = copy_reference_images_to_output_dir(opt_config, config, current_output_dir)
 
+    opt_config.sensors = opt_config.sensors[:len(ref_image_paths)]
+
     # 2. Optimize SDF compared to ref image(s)
     optimize_shape(opt_config, mts_args, ref_image_paths, current_output_dir, config)
 
